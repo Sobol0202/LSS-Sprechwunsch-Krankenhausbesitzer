0e5cd5761f80389e5823282b941b3ce9cc18aa9d
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-08-26 17:44:06 +0200

    Update LSS Krankenhausbesitzer.user.js
    -removed funktion that loads all owners on opening
    +added funktion that loads owners on hover
    +added Consol Outputs for debuggung
    -outcommented all Consoleoutputs

--- LSS Krankenhausbesitzer.user.js
+++ LSS Krankenhausbesitzer.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         LSS Krankenhausbesitzer
+// @name         LSS Sprechwunsch Krankenhausbesitzer
 // @namespace    www.leitstellenspiel.de
-// @version      1.0
-// @description  Zeigt den Namen des Besitzers eines Krankenhauses im Sprechwunsch an.
+// @version      1.1
+// @description  Zeigt den Namen des Besitzers eines Krankenhauses in der Sprechwunschliste an.
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/vehicles/*
 // @grant        none
@@ -11,53 +11,58 @@
 (function() {
     'use strict';
 
-    // Funktion zum Pausieren (Warten) für eine gegebene Anzahl von Millisekunden
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    // Hauptfunktion, die die Tabelle bearbeitet
-    async function processTable() {
-        // Alle Zeilen in der Tabelle auswählen
-        const rows = document.querySelectorAll('#alliance-hospitals tbody tr');
-        for (const row of rows) {
-            // Das Element mit der Gebäude-ID finden
-            const hospitalIdElement = row.querySelector('[id^="div_free_beds_"]');
-            if (hospitalIdElement) {
-                // Die Gebäude-ID aus dem Element extrahieren
-                const hospitalId = hospitalIdElement.id.replace('div_free_beds_', '');
-                // Eine Anfrage an die Gebäude-URL senden, um den Besitzer zu finden
-                const response = await fetch(`https://www.leitstellenspiel.de/buildings/${hospitalId}`);
-                const responseText = await response.text();
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(responseText, 'text/html');
-                // Den Namen des Besitzers auswählen, wenn vorhanden
-                const ownerNameElement = doc.querySelector('a[href^="/profile/"]');
-                if (ownerNameElement) {
-                    // Den Namen des Besitzers extrahieren und das Gebäudenamen-Element in der Zeile auswählen
-                    const ownerName = ownerNameElement.textContent.trim();
-                    const buildingNameElement = row.querySelector('td:first-child');
-                    // Den Besitzernamen als Tooltip setzen
-                    buildingNameElement.title = `Besitzer: ${ownerName}`;
-                }
-            }
-            // Eine kurze Pause von 100 ms einlegen, bevor die nächste Anfrage gesendet wird
-            await sleep(100);
+    async function showOwnerForHospital(buildingRow, hospitalId) {
+//        console.log('Fetching owner for hospital', hospitalId);
+        const response = await fetch(`https://www.leitstellenspiel.de/buildings/${hospitalId}`);
+        const responseText = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(responseText, 'text/html');
+        const ownerNameElement = doc.querySelector('a[href^="/profile/"]');
+        if (ownerNameElement) {
+            const ownerName = ownerNameElement.textContent.trim();
+            buildingRow.querySelector('td:first-child').title = `Besitzer: ${ownerName}`;
+//            console.log('Owner found:', ownerName);
         }
     }
 
-    // Beobachter erstellen, um auf Änderungen im DOM zu reagieren
-    const observer = new MutationObserver(() => {
-        // Die Zieltabelle auswählen
-        const targetTable = document.getElementById('alliance-hospitals');
-        if (targetTable) {
-            // Die Tabelle bearbeiten
-            processTable();
-            // Den Beobachter deaktivieren, nachdem die Tabelle bearbeitet wurde
-            observer.disconnect();
-        }
-    });
+    function attachHoverListeners(row) {
+        const hospitalIdElement = row.querySelector('[id^="div_free_beds_"]');
+        if (hospitalIdElement) {
+            const hospitalId = hospitalIdElement.id.replace('div_free_beds_', '');
+//            console.log('Attaching hover listener for row:', hospitalId);
 
-    // Den Beobachter starten, um Änderungen im DOM zu überwachen
-    observer.observe(document, { childList: true, subtree: true });
+            row.addEventListener('mouseenter', async function() {
+//                console.log('Mouse enter row:', hospitalId);
+                await showOwnerForHospital(row, hospitalId);
+            });
+
+            row.addEventListener('mouseleave', function() {
+//                console.log('Mouse leave row:', hospitalId);
+                row.querySelector('td:first-child').title = '';
+            });
+        }
+    }
+
+    const targetTable = document.getElementById('alliance-hospitals');
+    if (targetTable) {
+        const rows = targetTable.querySelectorAll('tbody tr');
+        rows.forEach(attachHoverListeners);
+
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.addedNodes) {
+                    const addedRows = Array.from(mutation.addedNodes)
+                        .filter(node => node.nodeName === 'TR' && node.querySelector('[id^="div_free_beds_"]'));
+
+                    addedRows.forEach(attachHoverListeners);
+                }
+            });
+        });
+
+        observer.observe(targetTable.querySelector('tbody'), { childList: true });
+    }
 })();
